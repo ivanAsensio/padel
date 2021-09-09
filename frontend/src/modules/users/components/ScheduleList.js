b2561ae9e103f2049726e23c1b6f60126b872830
Iván Asensio Vázquez <ivanAsensioInformatica@gmail.com>
2021-09-09 16:17:06 +0200

    Changed modal on add schedule and show user detaisl

--- frontend/src/modules/users/components/ScheduleList.js
+++ frontend/src/modules/users/components/ScheduleList.js
@@ -4,27 +4,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import * as selectors from '../selectors';
 import * as actions from '../actions';
 import {Errors} from '../../common';
+import Modal from 'react-bootstrap/Modal';
+import users from '../../users';
 
 const ScheduleList = (user) => {
 
     const schedules = useSelector(selectors.getAllSchedules);
+    const userObtained = useSelector(users.selectors.getUser);
     const [day, setDay] = useState("MONDAY");
     const [initHour, setInitHour] = useState('');
     const [finalHour, setFinalHour] = useState('');
     const [backendErrors, setBackendErrors] = useState(null);
-    const [showAdd, setShowAdd] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch();
+    const userRole = useSelector(users.selectors.getUserRole) !== "USER";
+    const showAdd = userRole || (user.user.id === userObtained.id);
 
     let form;
 
     const obtainMinutes = (hour) => {
         const hourSplitted = hour.split(':');
         return Number(hourSplitted[0]) * 60 + Number(hourSplitted[1]);
-    }
-
-    const handleAdd = (event) => {
-        event.preventDefault();
-        setShowAdd(true);
     }
 
     const handleSubmit = (event) => {
@@ -38,6 +38,7 @@ const ScheduleList = (user) => {
                 finalHour: obtainMinutes(finalHour)
             }
             dispatch(actions.addSchedule(user.user.id, schedule));
+            setShowModal(false);
             
         } else {
 
@@ -69,9 +70,9 @@ const ScheduleList = (user) => {
             <br></br>
             <h2 className="text-center font-weight-bold"><FormattedMessage id='project.scheduleList.title'/></h2>
             <br></br>
-            <form className="btn" onSubmit={(e) => handleAdd(e)}>
-                <button type="submit float-right"><FormattedMessage id='project.scheduleList.addSchedule'/></button>
-            </form>
+            { showAdd ?
+                <button type="submit float-right" onClick={(e) => setShowModal(true)}><FormattedMessage id='project.scheduleList.addSchedule'/></button>
+            : undefined}
             {schedules != null ? 
 
                 <div>
@@ -131,40 +132,51 @@ const ScheduleList = (user) => {
         
                 </div>
                 : <table className="table table-striped table-hover"></table>}
-                {showAdd ? 
-                <div>
-                        <select className="form-group row" defaultValue={day} onChange={e => onChangeSelect(e.target.value)}>
-                            <FormattedMessage id="project.global.day.monday">
-                                {(message) => <option value="MONDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.tuesday">
-                                {(message) => <option value="TUESDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.wednesday">
-                                {(message) => <option value="WEDNESDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.thursday">
-                                {(message) => <option value="THURSDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.friday">
-                                {(message) => <option value="FRIDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.saturday">
-                                {(message) => <option value="SATURDAY">{message}</option>}
-                            </FormattedMessage>
-                            <FormattedMessage id="project.global.day.sunday">
-                                {(message) => <option value="SUNDAY">{message}</option>}
-                            </FormattedMessage>
-                        </select>
-                        <input id="initHour" className="form-group row" type="text" onChange={e => setInitHour(e.target.value)} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$" placeholder="HH:HH"/>
-                        <input id="finalHour" className="form-group row" type="text" onChange={e => setFinalHour(e.target.value)} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$" placeholder="HH:MM"/>
-                        <form ref={node => form = node}
-                        className="needs-validation" noValidate 
-                        onSubmit={e => handleSubmit(e)}>
-                            <button type="submit">Añadir horario</button>
-                        </form>
-                </div>
-                : undefined}
+                {
+                <Modal show={showModal} onHide={() => setShowModal(false)}>
+                    <form ref={node => form = node}
+                                        className="needs-validation" noValidate 
+                                        onSubmit={e => handleSubmit(e)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title><FormattedMessage id="project.scheduleList.addSchedule"/></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                        <label><FormattedMessage id="project.global.fields.day"/></label>
+                            <select className="form-control" defaultValue={day} onChange={e => onChangeSelect(e.target.value)} required>
+                                <FormattedMessage id="project.global.day.monday">
+                                    {(message) => <option value="MONDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.tuesday">
+                                    {(message) => <option value="TUESDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.wednesday">
+                                    {(message) => <option value="WEDNESDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.thursday">
+                                    {(message) => <option value="THURSDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.friday">
+                                    {(message) => <option value="FRIDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.saturday">
+                                    {(message) => <option value="SATURDAY">{message}</option>}
+                                </FormattedMessage>
+                                <FormattedMessage id="project.global.day.sunday">
+                                    {(message) => <option value="SUNDAY">{message}</option>}
+                                </FormattedMessage>
+                            </select>
+                            <label><FormattedMessage id="project.global.fields.initHour"/></label>
+                            <input id="initHour" className="form-control" type="text" onChange={e => setInitHour(e.target.value)} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$" placeholder="HH:HH" required/>
+                            <label><FormattedMessage id="project.global.fields.finalHour"/></label>
+                            <input id="finalHour" className="form-control" type="text" onChange={e => setFinalHour(e.target.value)} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$" placeholder="HH:MM" required/>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <button type="submit" data-dismiss="modal"><FormattedMessage id="project.scheduleList.addSchedule"/></button>
+                        </Modal.Footer>
+                    </form>
+                </Modal>
+                
+                }
             </div>
 
     );
--- frontend/src/modules/users/components/UserDetails.js
+++ frontend/src/modules/users/components/UserDetails.js
@@ -9,15 +9,16 @@ import * as actions from '../actions';
 import {Errors} from '../../common';
 import {BackLink} from '../../common';
 import ScheduleList from './ScheduleList';
+import users from '../../users';
 
 const UserDetails = () => {
 
-    //const loggedIn = useSelector(users.selectors.isLoggedIn);
     const userObtained = useSelector(selectors.getUserObtained);
     const dispatch = useDispatch();
     const history = useHistory();
     const [backendErrors, setBackendErrors] = useState(null);
     const schedules = useSelector(selectors.getAllSchedules);
+    const userRole = useSelector(users.selectors.getUserRole) !== "USER";
 
     const {id} = useParams();
     let form;
@@ -62,48 +63,48 @@ const UserDetails = () => {
                                 <label className="p-2 font-weight-bold">
                                     <h1 className=" font-weight-bold">{userObtained.login && userObtained.login}</h1>
                                 </label>
-                                <form ref={node => form = node} className="needs-validation p-2 ml-auto" noValidate onSubmit={e => handleSubmit(e)}>                             
+                                {userRole && <form ref={node => form = node} className="needs-validation p-2 ml-auto" noValidate onSubmit={e => handleSubmit(e)}>                             
                                     <button type="submit" className="btn btn-primary">
                                         <FormattedMessage id="project.global.buttons.changeLevel"/> 
                                     </button>
-                                </form>
+                                </form>}
                             </div>
                         </div>
                         <br></br>
                         <br></br>
-                        <div className="row">
-                            <div className="text-center">
+                        <div className="row d-flex">
+                            <div className="p-2 text-center col-md-5">
                                 <img src={userObtained && userObtained.image ? userObtained.image : "http://ssl.gstatic.com/accounts/ui/avatar_2x.png"} className="rounded-circle img-fluid" alt="avatar"></img>
                             </div>
-                            <div className="tab-content">
+                            <div className="p-2 col-md-7">
                                 <div htmlFor="name" className="col-xs-6">
                                     <label htmlFor="name" className="col-md-12 col-form-label">
                                         <label className="font-weight-bold"><FormattedMessage id="project.global.fields.firstName"/></label>
-                                        :{userObtained.name && userObtained.name}
+                                        :&nbsp;{userObtained.name && userObtained.name}
                                     </label>
                                 </div>
                                 <div htmlFor="lastName">
                                     <label htmlFor="lastName" className="col-md-12 col-form-label">
                                         <label className="font-weight-bold"><FormattedMessage id="project.global.fields.lastNames"/></label>
-                                        :{userObtained.lastName1 && userObtained.lastName2 && userObtained.lastName1.concat(' ', userObtained.lastName2)}
+                                        :&nbsp;{userObtained.lastName1 && userObtained.lastName2 && userObtained.lastName1.concat(' ', userObtained.lastName2)}
                                     </label>
                                 </div>
                                 <div htmlFor="level" className="col-xs-6">
                                     <label htmlFor="level" className="col-md-12 col-form-label">
                                         <label className="font-weight-bold"><FormattedMessage id="project.global.fields.level"/></label>
-                                        :{userObtained.level && userObtained.level}
+                                        :&nbsp;{userObtained.level && userObtained.level}
                                     </label>
                                 </div>
                                 <div htmlFor="position" className="col-xs-6">
                                     <label htmlFor="position" className="col-md-12 col-form-label">
                                         <label className="font-weight-bold"><FormattedMessage id="project.global.fields.position"/></label>
-                                        :{userObtained.position && userObtained.position}
+                                        :&nbsp;{userObtained.position && userObtained.position}
                                     </label>
                                 </div>
                                 <div htmlFor="state" className="col-xs-6">
                                     <label htmlFor="state" className="col-md-12 col-form-label">
                                         <label className="font-weight-bold"><FormattedMessage id="project.global.fields.state"/></label>  
-                                        :{
+                                        :&nbsp;{
                                             userObtained.state ? 
                                                 <FormattedMessage id="project.global.fields.state.true"/>  
                                             :   <FormattedMessage id="project.global.fields.state.false"/>  
@@ -113,7 +114,7 @@ const UserDetails = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="ml-auto p-2">
+                    <div className="ml-auto p-2 col-md-5">
                         <ScheduleList schedules={schedules} user={userObtained}></ScheduleList>
                     </div>
                 </div>
